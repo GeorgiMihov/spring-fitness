089c08929c5521969b06ced74c6376bfe2775977
Georgi Mihov <g.mihov.r@gmail.com>
2022-05-30 22:57:43 +0300

    Create calulator endpoint

---
+++ src/main/java/org/services/project/courseproject/CalculatorEndpoint.java
@@ -0,0 +1,33 @@
+package org.services.project.courseproject;
+
+import io.spring.guides.gs_producing_web_service.CalculateCaloriesRequest;
+import io.spring.guides.gs_producing_web_service.CalculateCaloriesResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class CalculatorEndpoint {
+    private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+
+    private CalculatorRepository calculatorRepository;
+
+    @Autowired
+    public CalculatorEndpoint(CalculatorRepository calculatorRepository) {
+        this.calculatorRepository = calculatorRepository;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "calculateCaloriesRequest")
+    @ResponsePayload
+    public CalculateCaloriesResponse calculateCalories(@RequestPayload CalculateCaloriesRequest request) {
+        //if ()
+        int result = calculatorRepository.calculateCalories(request.getAge(), request.getHeight(), request.getWeight());
+
+        CalculateCaloriesResponse response = new CalculateCaloriesResponse();
+        response.setCalories(result);
+
+        return response;
+    }
+}
